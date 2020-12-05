a1b38894196ce4539916eb50c59e097962fe8019
Allar Allas <allar.allas@gmail.com>
2020-12-05 11:36:14 +0200

    Day 05, optimize

--- 05/aoc05.ts
+++ 05/aoc05.ts
@@ -1,25 +1,17 @@
 import {readFileSync} from 'fs';
 
-const seatIDs: number[] = [];
-
-function calc(code: string, max: number, inc: string, dec: string ) {
-    let min = 0;
-    for(let c of code) {
-        min = c === inc ? min + (max - min + 1) / 2 : min;
-        max = c === dec ? min + (max - min + 1) / 2 - 1 : max;
+function calc(code, min, max, inc) {
+    for (let c of code) {
+        c === inc ? min = min + (max - min + 1) / 2 : max = min + (max - min + 1) / 2 - 1;
     }
     return min;
 }
 
-readFileSync('./05/aoc05-input.txt', 'utf-8')
+const seatIDs = readFileSync('./05/aoc05-input.txt', 'utf-8')
     .trimEnd().split('\n')
-    .forEach((seat: string) => {
-        seatIDs.push(
-        calc(seat.slice(0,7),127,'B', 'F') * 8 +
-         calc(seat.slice(7,10),7,'R', 'L'));
-    });
+    .map(seat => calc(seat.slice(0, 7), 0, 127, 'B') * 8 +
+        calc(seat.slice(7, 10), 0, 7, 'R'));
 
-console.log(Math.max(...seatIDs));
-console.log(seatIDs
+console.log(Math.max(...seatIDs), seatIDs
     .sort((a, b) => a - b)
     .filter((value, index, array) => index > 0 && value - array[index - 1] > 1)[0] - 1);
