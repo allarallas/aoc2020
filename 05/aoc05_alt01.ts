ea13d1ab21a8b1dc130e9ce772ea98b3b90a6f6f
Allar Allas <allar.allas@gmail.com>
2020-12-05 17:37:08 +0200

    Day 05, Alternative solution

---
+++ 05/aoc05_alt01.ts
@@ -0,0 +1,9 @@
+import {readFileSync} from 'fs';
+
+const seatIDs = readFileSync('./05/aoc05-input.txt', 'utf-8').trimEnd()
+    .replace(/F|L/g, '0').replace(/B|R/g, '1').split('\n')
+    .map(seat => parseInt(seat.slice(0, 7), 2) * 8 + parseInt(seat.slice(7, 10), 2));
+
+console.log(Math.max(...seatIDs), seatIDs
+    .sort((a, b) => a - b)
+    .filter((v, i, a) => i > 0 && v - a[i - 1] > 1)[0] - 1);
