2e602863b37e61f10640cc4e6375267cbc4444cb
Allar Allas <allar.allas@gmail.com>
2020-12-06 12:06:43 +0200

    Day 06

---
+++ 06/aoc06.ts
@@ -0,0 +1,12 @@
+import {readFileSync} from 'fs';
+
+const input = readFileSync('./06/aoc06-input.txt', 'utf-8').trimEnd().split('\n\n');
+
+const p1 = input.reduce((p, c) => p + new Set(c.match(/\w/g)).size, 0);
+
+const p2 = input
+    .map(group => group.split('\n')
+        .reduce((p, c) => p.filter(v => c.indexOf(v) >= 0), group.split('\n')[0].split('')).length)
+    .reduce((p, c) => p + c);
+
+console.log('Misead result: ', p1, 'Correct result: ', p2);
