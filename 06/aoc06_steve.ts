297d80945f1417a70085c5842a0255db364065ae
Allar Allas <allar.allas@gmail.com>
2020-12-06 16:13:30 +0200

    Day 06 - better var names

--- 06/aoc06_steve.ts
+++ 06/aoc06_steve.ts
@@ -3,10 +3,10 @@
 
 import {readFileSync} from 'fs';
 
-const wtf = readFileSync('./06/aoc06-input.txt', 'utf-8').trimEnd().split('\n\n')
+const result = readFileSync('./06/aoc06-input.txt', 'utf-8').trimEnd().split('\n\n')
     .map(group => 0x10000 * group.split('\n')
             .reduce((p, c) => p.filter(v => c.indexOf(v) >= 0), group.split('\n')[0].split('')).length
         + new Set(group.match(/\w/g)).size
     ).reduce((p, c) => p + c)
 
-console.log('Misread result: ', wtf & 0xffff, 'Correct result: ', wtf >> 0x10);
+console.log('Misread result: ', result & 0xffff, 'Correct result: ', result >> 0x10);
