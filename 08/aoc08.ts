033b33a89c9ca545170191dd28259c966a6ed4d4
Allar Allas <allar.allas@gmail.com>
2020-12-08 12:51:03 +0200

    Day 08 - minor improvements

--- 08/aoc08.ts
+++ 08/aoc08.ts
@@ -1,33 +1,40 @@
 import {readFileSync} from 'fs';
 
-
-const origBuff = readFileSync('./08/aoc08-input-test-01.txt', 'utf-8').trimEnd().split('\n').map(line => {
+const origBuf = readFileSync('./08/aoc08-input-test-01.txt', 'utf-8').trimEnd().split('\n').map((line, index) => {
     const v = line.split(' ');
     return {
+        index: index,
         code: v[0],
         value: v[1],
         exec_count: 0
     }
 });
+const jmpBuf = origBuf.filter(value => value.code === 'jmp');
 
-function run(buff, i): [number, number] {
-    if (i >= 0) buff[i].code = 'nop';
+function run(buf, i): [number, number] {
+    if (i >= 0) buf[i].code = 'nop';
     let p = 0, acc = 0;
-    while (!(p > buff.length - 1) && !(buff[p].exec_count > 0)) {
-        buff[p].exec_count++;
-        switch (buff[p].code) {
+    while (!(p > buf.length - 1) && !(buf[p].exec_count > 0)) {
+        buf[p].exec_count++;
+        switch (buf[p].code) {
             case 'jmp': {
-                p += parseInt(buff[p].value);
+                p += parseInt(buf[p].value);
                 break;
             }
-            case 'acc': acc += parseInt(buff[p].value);
-            default: p++;
+            case 'acc':
+                acc += parseInt(buf[p].value);
+            default:
+                p++;
         }
     }
-    return [p > buff.length - 1 ? 1 : 0, acc];
+    return [p > buf.length - 1 ? 1 : 0, acc];
 }
 
-console.log(
-    'Faulty result: ', run(origBuff.map(e => ({...e})), -1),
-    'Corrected result: ', origBuff.map((v, i, array) =>
-        v.code === 'jmp' ? run(origBuff.map(e => ({...e})), i) : [0, 0]).filter(value => value[0] > 0)[0]);
+function correct(): number {
+    for (let instruction of jmpBuf) {
+        const result = run(origBuf.map(e => ({...e})), instruction.index);
+        if (result[0] > 0) return result[1];
+    }
+}
+
+console.log('Faulty result: ', run(origBuf.map(e => ({...e})), -1)[1], 'Corrected result: ', correct());
