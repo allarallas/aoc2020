1a06d3ea528d1659313248859a0a64839116a0ec
Allar Allas <allar.allas@gmail.com>
2020-12-09 10:23:30 +0200

    Day 09 - some optimisations

--- 09/aoc09.ts
+++ 09/aoc09.ts
@@ -4,10 +4,9 @@ const input = readFileSync('./09/aoc09-input.txt', 'utf-8').trimEnd().split('\n'
 const preLength = 25;
 
 function findFirstInvalid() {
-    for (let p = preLength; p < input.length - 1; p++) {
-        let matched = false;
-        for (let pp = p - preLength; pp < p; pp++)
-            for (let ppp = p - preLength; ppp < p; ppp++)
+    for (let p = preLength, matched = false; p < input.length - 1; p++, matched = false) {
+        for (let pp = p - preLength; pp < p && !matched; pp++)
+            for (let ppp = p - preLength; ppp < p && !matched; ppp++)
                 matched = matched || input[pp] + input[ppp] === input[p];
         if (!matched) return [p, input[p]];
     }
@@ -15,14 +14,12 @@ function findFirstInvalid() {
 
 function findContigAdditives(invalidNumber: number) {
     for (let p = 0; p < input.length - 2; p++)
-        if (input[p] !== invalidNumber) {
-            let sum = 0, nn = [];
-            for (let pp = p; pp < input.length - 1 && sum < invalidNumber; pp++) {
+        if (input[p] !== invalidNumber)
+            for (let pp = p, sum = 0, nn = []; pp < input.length - 1 && sum < invalidNumber; pp++) {
                 sum += input[pp];
                 nn.push(input[pp]);
+                if (sum === invalidNumber) return nn;
             }
-            if (sum === invalidNumber) return nn;
-        }
 }
 
 const firstInvalid = findFirstInvalid();
