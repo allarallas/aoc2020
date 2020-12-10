674a8ccb52d14619d08b5e04471ccb37f686101a
Allar Allas <allar.allas@gmail.com>
2020-12-10 14:30:41 +0200

    Day 10

---
+++ 10/aoc10.ts
@@ -0,0 +1,25 @@
+import {readFileSync} from 'fs';
+
+let input = readFileSync('./10/aoc10-input.txt', 'utf-8').trimEnd().split('\n').map(v => parseInt(v)).sort((a, b) => a - b);
+input = [0, ...input, input[input.length - 1] + 3];
+
+const diffs = [];
+for (let n = 1; n < input.length; n++) {
+    const diff = input[n] - input[n - 1];
+    diffs.push(diff);
+}
+
+const streaks = [];
+for (let i = 0, p = 0; i < diffs.length - 1; i++, p = i) {
+    while (diffs[i + 1] === 1 && diffs[i] === 1) i++;
+    if (i !== p) streaks.push(i - p + 1);
+}
+console.log('1 x 3:',
+    diffs.filter(v => v === 1).length * diffs.filter(v => v === 3).length,
+    ', omg:',
+    streaks.map((v: number) => {
+        let p = 1;
+        for (let i = 1; i < v; i++) p += i;
+        return p;
+    }).reduce((a: number, v: number) => a * v, 1)
+);
