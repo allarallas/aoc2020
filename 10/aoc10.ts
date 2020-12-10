b7ab4471b02358228cf3f1c9131292628d10005a
Allar Allas <allar.allas@gmail.com>
2020-12-10 14:34:03 +0200

    Day 10 - minor touches

--- 10/aoc10.ts
+++ 10/aoc10.ts
@@ -4,22 +4,21 @@ let input = readFileSync('./10/aoc10-input.txt', 'utf-8').trimEnd().split('\n').
 input = [0, ...input, input[input.length - 1] + 3];
 
 const diffs = [];
-for (let n = 1; n < input.length; n++) {
-    const diff = input[n] - input[n - 1];
-    diffs.push(diff);
-}
+for (let n = 1; n < input.length; n++)
+    diffs.push(input[n] - input[n - 1]);
 
 const streaks = [];
 for (let i = 0, p = 0; i < diffs.length - 1; i++, p = i) {
     while (diffs[i + 1] === 1 && diffs[i] === 1) i++;
     if (i !== p) streaks.push(i - p + 1);
 }
+
 console.log('1 x 3:',
     diffs.filter(v => v === 1).length * diffs.filter(v => v === 3).length,
     ', omg:',
-    streaks.map((v: number) => {
+    streaks.map(v => {
         let p = 1;
         for (let i = 1; i < v; i++) p += i;
         return p;
-    }).reduce((a: number, v: number) => a * v, 1)
+    }).reduce((a, v) => a * v, 1)
 );
